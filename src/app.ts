5bfdce392bcbbb06aab05da24f44e7bd1714392c
Abel Shibabaw <abelshibabaw291@gmail.com>
2024-07-31 16:03:26 +0300

    feat: added schema definition for the blog application

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,22 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
-
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 app.listen(port, () => {
   console.log(`⚡️Server is running at http://localhost:${port}`);
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
 });
